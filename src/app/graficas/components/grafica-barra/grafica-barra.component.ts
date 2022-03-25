597ebd937cfb0563df31dd5476ea76256a19528d
WiiruxCruz <hialf.92@gmail.com>
2022-03-25 02:00:58 -0600

    Creación de componente personalizado

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
 
@@ -10,23 +10,35 @@ import { Label } from 'ng2-charts';
 })
 export class GraficaBarraComponent implements OnInit {
 
+	@Input() horizontal: boolean = false;
+
 	public barChartOptions: ChartOptions = {
 		responsive: true
 	};
 
-	public barChartLabels: Label[] = [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ]
+	@Input() barChartLabels: Label[] = [
+		//'2020', '2021', '2022', '2023', '2024', '2025', '2026'
+	];
 	public barChartType: ChartType = 'bar';
 	public barChartLegend: true;
 
-	public barChartData: ChartDataSets[] = [
+	@Input() barChartData: ChartDataSets[] = [
+		/*
 		{ data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#F05C4D', hoverBackgroundColor: 'red' },
 		{ data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#C20CF7', hoverBackgroundColor: 'red' },
 		{ data: [ 12, 48, 51, 19, 57, 23, 33 ], label: 'Series C', backgroundColor: '#2CED0C', hoverBackgroundColor: 'red' }
+		*/
 	];
 
-	constructor() { }
+	constructor() {
+	}
 
 	ngOnInit(): void {
+		console.log( this.horizontal );
+
+		if( this.horizontal ) {
+			this.barChartType = 'horizontalBar';
+		}
 	}
 
 }
