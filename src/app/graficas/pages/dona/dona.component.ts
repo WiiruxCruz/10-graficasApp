f86b4dc835c8a31789c28ad5df0a79a09cc85e18
WiiruxCruz <hialf.92@gmail.com>
2022-03-25 02:30:22 -0600

    Creación de gráfica de Dona mediante petición Http

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
 
 @Component({
   selector: 'app-dona',
@@ -6,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DonaComponent implements OnInit {
+export class DonaComponent {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	// Doughnut
+	public doughnutChartLabels: Label[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others' ];
+	public doughnutChartData: MultiDataSet = [
+			 [ 350, 450, 100, 150 ]
+			 //[ 50, 150, 120 ] ,
+			 //[ 250, 130, 70 ]
+	];
+	public doughnutChartType: ChartType = 'doughnut';
+
+	public colors: Color[] = [
+		{
+			backgroundColor: [
+				'#F05C4D',
+				'#C20CF7',
+				'#2CED0C',
+				'#0029E0'
+			]
+		}
+	]
 
 }
