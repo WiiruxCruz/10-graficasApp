faba209dfec64e15d6a22eff643b014fd15272bf
WiiruxCruz <hialf.92@gmail.com>
2022-03-25 02:40:38 -0600

    Consumir servicio de redes sociales

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { GraficasService } from '../../services/graficas.service';
 
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html',
@@ -9,14 +12,44 @@ import { GraficasService } from '../../services/graficas.service';
 })
 export class DonaHttpComponent implements OnInit {
 
-  constructor( private graficasService: GraficasService) { }
+	  // Doughnut
+	public doughnutChartLabels: Label[] = [
+		//'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Others'
 
-  ngOnInit(): void {
-	this.graficasService.getUsuariosRedesSociales()
-	.subscribe(
-		data => {
-			console.log(data);
-		}
-	)
-  }
+	];
+	public doughnutChartData: MultiDataSet = [
+		//[ 350, 450, 100, 150 ]
+	];
+	public doughnutChartType: ChartType = 'doughnut';
+
+	public colors: Color[] = [
+	{
+		backgroundColor: [
+			'#F05C4D',
+			'#C20CF7',
+			'#2CED0C',
+			'#0029E0'
+		]
+	}
+	]
+
+	constructor( private graficasService: GraficasService) { }
+
+	ngOnInit(): void {
+		this.graficasService.getUsuariosRedesSociales()
+		.subscribe(
+			data => {
+				console.log(data);
+				const labels = Object.keys( data );
+				const values = Object.values( data );
+				console.log(labels);
+				console.log(values);
+
+				this.doughnutChartLabels = labels;
+				this.doughnutChartData.push( values );
+
+			}
+		)
+	}
+
 }
